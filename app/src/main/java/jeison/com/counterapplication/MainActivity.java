fd7ebdf5f96b0389706c24f26ece6a6dca8badb4
Jeison <jei_son1516@hotmail.com>
2021-12-24 02:27:58 -0500

    welocome screen xml

--- app/src/main/java/jeison/com/counterapplication/MainActivity.java
+++ app/src/main/java/jeison/com/counterapplication/MainActivity.java
@@ -3,12 +3,20 @@ package jeison.com.counterapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_main);
+
     }
 }
