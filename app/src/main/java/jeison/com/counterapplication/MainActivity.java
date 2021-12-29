2ce5d99d01d8790049f1a3bc2e54244f8a542a91
Jeison <jei_son1516@hotmail.com>
2021-12-28 19:34:22 -0500

    button text on dark theme fix

--- app/src/main/java/jeison/com/counterapplication/MainActivity.java
+++ app/src/main/java/jeison/com/counterapplication/MainActivity.java
@@ -11,11 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Remove title bar
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        //Remove notification bar
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_main);
 
     }
