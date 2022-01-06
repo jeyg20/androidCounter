724d1df22d9454cee1dc4b906d905b1152a7604b
Jeison <jei_son1516@hotmail.com>
2022-01-06 12:41:48 -0500

    functionality added to main activity button

--- app/src/main/java/jeison/com/counterapplication/MainActivity.java
+++ app/src/main/java/jeison/com/counterapplication/MainActivity.java
@@ -2,9 +2,13 @@ package jeison.com.counterapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_main);
 
+       Button button = (Button) findViewById(R.id.button_main_start);
+       button.setOnClickListener(new View.OnClickListener() {
+
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(getApplicationContext() , homeActivity.class);
+                startActivity(intent);
+           }
+       });
+
     }
 }
+
