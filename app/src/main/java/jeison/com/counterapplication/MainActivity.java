a575936f87a8ef211b22aff87dceb66802c3e273
Jeison <jei_son1516@hotmail.com>
2022-01-09 18:37:47 -0500

    removed unused code

--- app/src/main/java/jeison/com/counterapplication/MainActivity.java
+++ app/src/main/java/jeison/com/counterapplication/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_main);
 
     }
