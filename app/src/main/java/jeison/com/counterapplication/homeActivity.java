2af76a66e9e4a254c863ef70d1834842a1a8593c
Jeison <jei_son1516@hotmail.com>
2022-01-31 17:09:49 -0500

    Setting home screen recycler view adapter and counters info

--- app/src/main/java/jeison/com/counterapplication/homeActivity.java
+++ app/src/main/java/jeison/com/counterapplication/homeActivity.java
@@ -1,21 +1,49 @@
 package jeison.com.counterapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import jeison.com.counterapplication.adapter.CounterAdapter;
+import jeison.com.counterapplication.model.Counter;
+
 public class homeActivity extends AppCompatActivity {
-
-
-    private loadingFragment loadingFragment = new loadingFragment();
+    private ArrayList<Counter> countersList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        recyclerView  = findViewById(R.id.rvCounters);
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.home_fragment, loadingFragment).commit();
+        countersList = new ArrayList<>();
+
+        setDef();
+        setAdapter();
+        
+    }
+
+    private void setAdapter() {
+        CounterAdapter adapter = new CounterAdapter(countersList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setDef() {
+        countersList.add(new Counter("Cups of coffee", 0));
+        countersList.add(new Counter("Records played", 0));
+        countersList.add(new Counter("Number of times I’ve forgotten my mother’s name " +
+                "because I was high on Frugelés.", 0));
+        countersList.add(new Counter("Apples eaten", 0));
+
     }
 }
